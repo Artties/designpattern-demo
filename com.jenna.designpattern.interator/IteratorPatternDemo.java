3bccbad8db6c017c4c01de903b1dddda624fc6f6
Jenna He <hecncn@126.com>
2022-01-21 18:18:31 +0800

    update script

---
+++ com.jenna.designpattern.interator/IteratorPatternDemo.java
@@ -0,0 +1,13 @@
+package com.jenna.designpattern.iterator;
+
+import java.util.Iterator;
+
+public class IteratorPatternDemo {
+    public interface Iterator{
+        public abstract boolean hasNext();
+        
+    }
+    public interface Aggreate{
+        public abstract Iterator iterator();
+    }
+}
--- com.jenna.designpattern.interator/WithoutIteratorPatternDemo.java
+++ com.jenna.designpattern.interator/WithoutIteratorPatternDemo.java
@@ -1,4 +1,8 @@
 package com.jenna.designpattern.iterator;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /*
 * 不使用设计模式
 *
@@ -9,15 +13,22 @@ public class WithoutIteratorPatternDemo {
         Student student1 = new Student("小明");
         Student student2 = new Student("小王");
 
-        Student[] students = new Student[2];
-        students[0] = student1;
-        students[1] = student2;
+//        Student[] students = new Student[2];
+//        students[0] = student1;
+//        students[1] = student2;
+        Map<String,Student> students = new HashMap<String,Student>();
+        students.put(student1.getName(), student1);
+        students.put(student2.getName(),student2);
 
         Classroom classroom = new Classroom();
         classroom.setStudents(students);
 
-        Student[] resultStudents = classroom.getStudents();
-        for (Student resultStudent:resultStudents){
+//        Student[] resultStudents = classroom.getStudents();
+//        for (Student resultStudent:resultStudents){
+//            System.out.println(resultStudents);
+//        }
+        Map<String,Student> resultStudents = classroom.getStudents();
+        for (Student resultStudent : resultStudents.values()){
             System.out.println(resultStudents);
         }
 
@@ -26,7 +37,6 @@ public class WithoutIteratorPatternDemo {
         private String name;
         public Student(String name){
             super();
-            this.name = name;
         }
         public String getName(){
             return name;
@@ -35,16 +45,27 @@ public class WithoutIteratorPatternDemo {
             this.name = name;
         }
     }
-    /*
-     * 教室类
-     * */
-    public static class Classroom{
-        private Student[] students;
+//    /*
+//     * 教室类
+//     * */
+//    public static class Classroom{
+//        private Student[] students;
+//
+//        public Student[] getStudents(){
+//            return students;
+//        }
+//        public void setStudents(Student[] students){
+//            this.students = students;
+//        }
+//    }
 
-        public Student[] getStudents(){
+        public static class Classroom{
+        private Map<String,Student> students;
+
+        public Map<String,Student> getStudents(){
             return students;
         }
-        public void setStudents(Student[] students){
+        public void setStudents(Map<String,Student> students){
             this.students = students;
         }
     }
